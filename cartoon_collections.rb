f91d38bfd9f68172e0f53795bf47a20ce7c6e736
KG ZZ <czheng5h88@gmail.com>
2019-09-10 20:46:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(array)
   }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  
 end
 
 def find_the_cheese# code an argument here
