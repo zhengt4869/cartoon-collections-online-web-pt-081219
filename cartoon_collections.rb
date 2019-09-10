077a7b884330ee0d17a20cb7cac4a45a6e8e5e33
KG ZZ <czheng5h88@gmail.com>
2019-09-10 20:50:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,11 @@ def long_planeteer_calls(array)# code an argument here
   false 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each{|cheese|
+    if cheese_types.include?
+  }
+  nil
 end
