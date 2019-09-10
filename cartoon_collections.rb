231eee137df5d55222b394bf427e266946aed3fc
KG ZZ <czheng5h88@gmail.com>
2019-09-10 20:48:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,14 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)# code an argument here
-  
+  i = 0 
+  while i < array.length 
+    if array[i].length > 4
+      return true 
+    end 
+    i+=1 
+  end 
+  false 
 end
 
 def find_the_cheese# code an argument here
