2178cec32f4d575d0b34c269f1faed56154f7c3a
KG ZZ <czheng5h88@gmail.com>
2019-09-10 20:39:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect{||}
+  array.collect{|word|
+    first = word.shift
+  }
 end
 
 def long_planeteer_calls# code an argument here
