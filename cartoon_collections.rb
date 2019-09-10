fbdff14381a5c2901abb76fee52025b823c63750
KG ZZ <czheng5h88@gmail.com>
2019-09-10 20:50:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,9 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.each{|cheese|
-    if cheese_types.include?
+    if cheese_types.include?(cheese)
+      return cheese
+    end
   }
   nil
 end
